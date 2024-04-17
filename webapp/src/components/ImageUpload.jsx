a2c484389688d2c7a0adf9a2acdd8e4d597be1aa
Chethan M <46964474+chethan9066@users.noreply.github.com>
2024-04-17 05:35:28 +0000

    redesigned to row wise

--- webapp/src/components/ImageUpload.jsx
+++ webapp/src/components/ImageUpload.jsx
@@ -17,16 +17,16 @@ const ImageUpload = () => {
     formData.append('image', file);
 
     try {
-    //   const response = await axios.post('/upload', formData, {
-    //     headers: {
-    //       'Content-Type': 'multipart/form-data',
-    //     },
-    //   });
+      //   const response = await axios.post('/upload', formData, {
+      //     headers: {
+      //       'Content-Type': 'multipart/form-data',
+      //     },
+      //   });
 
-    //   setImageData(response.data.image);
-    //   setOutput(response.data.output);
-        setImageData(file);
-        setOutput(file);
+      //   setImageData(response.data.image);
+      //   setOutput(response.data.output);
+      setImageData(file);
+      setOutput(file);
     } catch (error) {
       console.error('Error uploading image:', error);
     } finally {
@@ -36,46 +36,47 @@ const ImageUpload = () => {
 
   return (
     <div className="container">
-      <div className="columns is-centered" style={{marginTop:"10px"}}>
-        <div className="column is-half">
-          <div className="box">
-            <h5 className="title is-5">Input Image</h5>
-            {file && (
-              <figure className="image is-square">
-                <img src={URL.createObjectURL(file)} alt="Uploaded" />
-              </figure>
-            )}
-            <div className="file has-name is-fullwidth">
-              <label className="file-label">
-                <input
-                  className="file-input"
-                  type="file"
-                  accept="image/*"
-                  onChange={handleFileChange}
-                />
-                <span className="file-cta">
-                  <span className="file-icon">
-                    <i className="fas fa-upload"></i>
-                  </span>
-                  <span className="file-label">Choose a file...</span>
-                </span>
-                <span className="file-name"></span>
-              </label>
-            </div>
-            <button
-              className={`button is-link is-fullwidth ${
-                isLoading ? 'is-loading' : ''
-              }`}
-              onClick={handleUpload}
-              disabled={isLoading || !file}
-            >
-              {isLoading ? 'Uploading...' : 'SUBMIT'}
-            </button>
-          </div>
-          <div className="box">
-            <h5 className="title is-5">OUTPUT</h5>
-            {isLoading && <progress className="progress is-small is-primary" max="100"></progress>}
-            {/* {Object.keys(output).map((key) => (
+      <div style={{ marginTop: "10px" }}>
+        <div >
+          <div className="fixed-grid">
+            <div className='grid'>
+              <div className="box">
+                <h5 className="title is-5">Input Image</h5>
+                {file && (
+                  <figure className="image is-square">
+                    <img src={URL.createObjectURL(file)} alt="Uploaded" />
+                  </figure>
+                )}
+                <div className="file has-name is-fullwidth">
+                  <label className="file-label">
+                    <input
+                      className="file-input"
+                      type="file"
+                      accept="image/*"
+                      onChange={handleFileChange}
+                    />
+                    <span className="file-cta">
+                      <span className="file-icon">
+                        <i className="fas fa-upload"></i>
+                      </span>
+                      <span className="file-label">Choose a file...</span>
+                    </span>
+                    <span className="file-name"></span>
+                  </label>
+                </div>
+                <button
+                  className={`button is-link is-fullwidth ${isLoading ? 'is-loading' : ''
+                    }`}
+                  onClick={handleUpload}
+                  disabled={isLoading || !file}
+                >
+                  {isLoading ? 'Uploading...' : 'SUBMIT'}
+                </button>
+              </div>
+              <div className="box">
+                <h5 className="title is-5">OUTPUT</h5>
+                {isLoading && <progress className="progress is-small is-primary" max="100"></progress>}
+                {/* {Object.keys(output).map((key) => (
               <div key={key} className="level">
                 <div className="level-left">
                   <div className="level-item">
@@ -95,11 +96,13 @@ const ImageUpload = () => {
                 </div>
               </div>
             ))} */}
-            {imageData && (
-              <figure className="image is-square">
-                <img src={URL.createObjectURL(imageData)} alt="output image" />
-              </figure>
-            )}
+                {imageData && (
+                  <figure className="image is-square">
+                    <img src={URL.createObjectURL(imageData)} alt="output image" />
+                  </figure>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
